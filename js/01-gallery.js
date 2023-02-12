216ed4238f1e82fc7d62a8a7ab854a5ec04e21e0
NikitaDima <onekill13@gmail.com>
2023-02-12 02:29:18 +0200

    перше завдання

---
+++ js/01-gallery.js
@@ -0,0 +1,45 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const wrapperEl = document.querySelector('.gallery');
+
+const galleryListEl = galleryItems.map(({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
+  `
+}).join('');
+
+wrapperEl.innerHTML = galleryListEl;
+
+wrapperEl.addEventListener('click', showClickImg);
+
+function showClickImg(evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`)
+
+instance.show()
+
+wrapperEl.addEventListener('keydown', evt => {
+    if (evt.code === 'Escape') {
+        instance.close()
+    }
+})
+}
