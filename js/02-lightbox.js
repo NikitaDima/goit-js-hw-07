c31f41fca420e43526541a93cfc72fda1bfc706a
NikitaDima <onekill13@gmail.com>
2023-02-12 02:55:12 +0200

    розмітка завдання 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const wrapperEl = document.querySelector('.gallery');
+
+const galleryListEl = galleryItems.map(({preview, original, description}) => {
+    return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>
+  `
+}).join('');
+
+wrapperEl.innerHTML = galleryListEl;
